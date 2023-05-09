d8f576b31811d1eaf355d217cf093acfc9db74b2
AuraSyncOficial <120164145+AuraSyncOficial@users.noreply.github.com>
2023-05-08 19:34:19 -0500

    Eddi Velásquez Lozano
    Se corrigieron errores

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-<<<<<<< HEAD
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Destroy(gameObject, 1f);
-=======
     protected Rigidbody rb;
     void Awake()
     {
@@ -25,6 +13,5 @@ public class Bullet : MonoBehaviour
     protected virtual void Update()
     {
         rb.velocity = Vector3.forward;
->>>>>>> 9b8ec2442179c04b25908d9e42bf3596ab5be646
     }
 }
--- Assets/Scripts/Enemy1.cs
+++ Assets/Scripts/Enemy1.cs
@@ -62,9 +62,6 @@ public class Enemy1 : MonoBehaviour
         }
     }
 
-
-
-
 }
 
 
